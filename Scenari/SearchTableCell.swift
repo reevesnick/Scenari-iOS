5e112372dd99ec5c36b99dc381a3b6bdc51586cf
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-06-11 01:37:34 -0400

    Added Search Feature
    -Added Fabric Custom Analytics
    -Added Facebook Login, works (sort of)

---
+++ Scenari/SearchTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchTableCell.swift
+//  Scenari
+//
+//  Created by Neegbeah Reeves on 6/10/16.
+//  Copyright © 2016 Brown Box Works. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableCell: PFTableViewCell {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Scenari/SearchTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchTableViewController.swift
+//  Scenari
+//
+//  Created by Neegbeah Reeves on 6/10/16.
+//  Copyright © 2016 Brown Box Works. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: PFQueryTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
