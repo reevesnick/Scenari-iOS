b3759bdce542ad2fb6c3c6935d9e71eb7a5783b9
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-12-23 13:00:35 -0500

    Inintal Target Extension of Apple Watch

---
+++ Scenari Apple Watch Extension/InterfaceController.swift
@@ -0,0 +1,32 @@
+//
+//  InterfaceController.swift
+//  Scenari Apple Watch Extension
+//
+//  Created by Neegbeah Reeves on 12/18/16.
+//  Copyright © 2016 Brown Box Works. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+//import ParseUI
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
